c70808af04588f94c32db40cd2f68057ca9acd45
Elizabeth Nguyen <enguyen15@huskers.unl.edu>
2021-08-26 16:14:13 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Elizabeth Nguyen
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main (int argc, char **argv) {
+
+     printf("Elizabeth Nguyen!\n");
+
+     return 0;
+ }
